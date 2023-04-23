a7d87f42c443d7733154b842b68b6a17a4c46da1
razbainyks <spobedinskas@gmail.com>
2023-04-23 12:02:49 +0300

    Python Games

---
+++ hangman.py
@@ -0,0 +1,67 @@
+import random
+HANGMAN_PICS = ['''
+  +---+
+      |
+      |
+      |
+     ===''', '''
+  +---+
+  O   |
+      |
+      |
+     ===''', '''
+  +---+
+  O   |
+  |   |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|   |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+ /    |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+ / \  |
+     ===''']
+words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+
+def getRandomWord(wordList):
+    #This function returns a random string from the passed list of strings.
+    wordIndex = random.randint(0,len(wordList) - 1)
+    return wordList[wordIndex]
+
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_PICS[len(missedLetters)])
+    print()
+
+    print('Missed letters:',end='')
+    for letter in missedLetters:
+        print(letter,end='')
+    print()
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)): #Replace blanks with correctly guessed letters.
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks: #show the secret word with space in between each letter
+        print(letter,end='')
+    print()
+
+def getGuess(alreadyGuessed):
+    {
+
+    }
