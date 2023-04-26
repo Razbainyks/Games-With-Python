aca145a51a3d7f747daba2f6734bb80a7a840806
razbainyks <spobedinskas@gmail.com>
2023-04-26 21:11:51 +0300

    save

--- hangman.py
+++ hangman.py
@@ -34,13 +34,31 @@ HANGMAN_PICS = ['''
   O   |
  /|\  |
  / \  |
-     ===''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+     ===''','''
++---+
+[O   |
+/|\  |
+/ \  |
+    ===''', '''
++---+
+[O]  |
+/|\  |
+/ \  |
+===''']
 
-def getRandomWord(wordList):
-    # This function returns a random string from the passed list of strings.
-    wordIndex = random.randint(0, len(wordList) - 1)
-    return wordList[wordIndex]
+
+
+words = {'Colors':'red orange yellow green blue indigo violet white black brown'.split(),
+	   'Shapes':'square triangle rectangle circle ellipse rhombus trapezoid chevron pentagon hexagon septagon octagon'.split(),
+	   'Fruits':'apple orange lemon lime pear watermelon grape grapefruit cherry banana cantaloupe mango strawberry tomato'.split(),
+	   'Animals':'bat bear beaver cat cougar crab deer dog donkey duck eagle fish frog goat leech lion lizard monkey moose mouse otter owl panda python rabbit rat shark sheep skunk squid tiger turkey turtle weasel whale wolf wombat zebra'.split()}
+
+def getRandomWord(wordDict):
+    # This function returns a random string from the passed dictionary of lists of strings and its key.
+    wordKey = random.choice(list(wordDict.keys()))
+    # Second, randomly select a word from the key's list in the dictionary:
+    wordIndex = random.randint(0, len(wordDict[wordKey])-1)
+    return [wordDict[wordKey][wordIndex],wordKey]
 
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
@@ -83,6 +101,20 @@ def playAgain():
 
 
 print('H A N G M A N')
+
+difficulty =''
+while difficulty not in 'EMH':
+    print('Enter difficulty: E - Easy, M - Medium, H - Hard')
+    difficulty = input().upper()
+if difficulty == 'M':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+if difficulty == 'H':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+    del HANGMAN_PICS[5]
+    del HANGMAN_PICS[3]
+
 missedLetters = ''
 correctLetters = ''
 secretWord = getRandomWord(words)
